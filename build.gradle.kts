019663b264742950e72c7a326a62efe3ea08bd29
imsyf <alghozalyi@gmail.com>
2022-10-17 11:18:34 +0700

    Set up Spotless lint plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,29 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "7.3.0" apply false
     id("com.android.library") version "7.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+    id("com.diffplug.spotless") version "6.7.2"
+}
+
+subprojects {
+    pluginManager.apply("com.diffplug.spotless")
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("$buildDir/**/*.kt", "bin/**/*.kt")
+            ktlint("0.45.2").userData(mapOf("android" to "true"))
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+        format("misc") {
+            target("**/*.kts", "**/*.gradle", "**/*.xml", "**/*.md", "**/.gitignore")
+            targetExclude("**/build/**/*.kts", "**/build/**/*.xml")
+            trimTrailingWhitespace()
+            indentWithSpaces(4)
+            endWithNewline()
+        }
+    }
 }
